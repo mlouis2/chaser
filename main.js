3fe842342c1b2075d9a7c26a5be282177c729846
Maddie Louis <MaddieLouis@Maddies-MacBook-Air.local>
2017-12-01 11:04:32 -0800

    Getting game over sound to play

--- main.js
+++ main.js
@@ -27,15 +27,14 @@ let scoreText = document.getElementById("score");
 let highScoreText = document.getElementById("highscore");
 let numSpawn = 1;
 
+//SOURCE: https://www.youtube.com/watch?v=vJaAy3jmEx8
+let gameOverSound = new Audio('https://raw.githubusercontent.com/mlouis2/chaser/master/sounds/GameOver.mp3');
 //SOURCE: https://freesound.org/people/timgormly/sounds/170155/
 let starSound = new Audio('https://raw.githubusercontent.com/mlouis2/chaser/master/sounds/Star.mp3');
-
 //SOURCE: https://www.youtube.com/watch?v=J-21BggTCbA
 let healthSound = new Audio('https://raw.githubusercontent.com/mlouis2/chaser/master/sounds/Powerup.mp3');
-
 //SOURCE: https://www.youtube.com/watch?v=MzZJQtUekwI
 let backgroundSounds = new Audio('https://raw.githubusercontent.com/mlouis2/chaser/master/sounds/Background.mp3');
-
 //SOURCE: https://www.youtube.com/watch?v=lpqDphtOskU
 let skeletonSounds = new Audio('https://raw.githubusercontent.com/mlouis2/chaser/master/sounds/skeleton.mp3');
 
@@ -346,6 +345,7 @@ function loadPauseScreen() {
 
 function endGame() {
 	pauseSounds();
+	gameOverSound.play();
 	gameOver = true;
 	ctx.font = "120px VT323";
 	ctx.fillStyle = "white";
