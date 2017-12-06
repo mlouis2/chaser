34444bebb97a9a366916c0567f0c85980cf09c86
Maddie Louis <MaddieLouis@Maddies-MacBook-Air.local>
2017-12-05 16:44:44 -0800

    Pushing changes

--- main.js
+++ main.js
@@ -11,25 +11,15 @@ const healthBar = document.querySelector("progress");
 const POWERUP_SIZE = 50;
 
 //SOURCE: https://www.youtube.com/watch?v=vJaAy3jmEx8
-let gameOverSound = new Audio(
-  "https://raw.githubusercontent.com/mlouis2/chaser/master/sounds/GameOver.mp3"
-);
+let gameOverSound = new Audio("https://raw.githubusercontent.com/mlouis2/chaser/master/sounds/GameOver.mp3");
 //SOURCE: https://freesound.org/people/timgormly/sounds/170155/
-let starSound = new Audio(
-  "https://raw.githubusercontent.com/mlouis2/chaser/master/sounds/Star.mp3"
-);
+let starSound = new Audio("https://raw.githubusercontent.com/mlouis2/chaser/master/sounds/Star.mp3");
 //SOURCE: https://www.youtube.com/watch?v=J-21BggTCbA
-let healthSound = new Audio(
-  "https://raw.githubusercontent.com/mlouis2/chaser/master/sounds/Powerup.mp3"
-);
+let healthSound = new Audio("https://raw.githubusercontent.com/mlouis2/chaser/master/sounds/Powerup.mp3");
 //SOURCE: https://www.youtube.com/watch?v=MzZJQtUekwI
-let backgroundSounds = new Audio(
-  "https://raw.githubusercontent.com/mlouis2/chaser/master/sounds/Background.mp3"
-);
+let backgroundSounds = new Audio("https://raw.githubusercontent.com/mlouis2/chaser/master/sounds/Background.mp3");
 //SOURCE: https://www.youtube.com/watch?v=lpqDphtOskU
-let skeletonSounds = new Audio(
-  "https://raw.githubusercontent.com/mlouis2/chaser/master/sounds/skeleton.mp3"
-);
+let skeletonSounds = new Audio("https://raw.githubusercontent.com/mlouis2/chaser/master/sounds/skeleton.mp3");
 
 var backgroundImage = new Image();
 backgroundImage.src = "https://image.ibb.co/gF02nm/game_background2.jpg";
